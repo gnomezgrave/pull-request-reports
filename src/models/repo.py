6cae05dfefa801c1e49182e61a524c2a975418f8
Praneeth Peiris <gnomez.grave@gmail.com>
2020-02-12 10:12:42 +0100

    Remove typejinting (because it can create cyclic dependencies)

--- src/models/repo.py
+++ src/models/repo.py
@@ -1,10 +1,14 @@
-from github import Repository
+from models import Pull
 
 
 class Repo:
-    def __init__(self, git_repo: Repository):
+    def __init__(self, git_repo):
         self._repo = git_repo
         self.name = git_repo.name
         self.owner = git_repo.owner
         self.created_at = git_repo.created_at
         self.updated_at = git_repo.updated_at
+
+    def get_open_pulls(self):
+        for pull in self._repo.get_pulls():
+            yield Pull(pull)
