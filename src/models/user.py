56e5f785111af26192015e2a06abb739841908be
Praneeth Peiris <gnomez.grave@gmail.com>
2020-03-02 09:05:33 +0100

    update the models

--- src/models/user.py
+++ src/models/user.py
@@ -2,4 +2,5 @@ class User:
     def __init__(self, git_user):
         self._user = git_user
         self.user_name = git_user.login
+        self.login = git_user.login
         self.display_name = git_user.name
