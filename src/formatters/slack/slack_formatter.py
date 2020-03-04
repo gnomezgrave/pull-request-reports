43460893b097b3228776d912574073fcbd178680
Praneeth Peiris <gnomez.grave@gmail.com>
2020-03-04 11:27:01 +0100

    Send to SNS

--- src/formatters/slack/slack_formatter.py
+++ src/formatters/slack/slack_formatter.py
@@ -6,15 +6,21 @@ class SlackFormatter:
         self._name_formatter = name_formatter
 
     def format_repos(self, repos):
+        repo_messages = []
+        repo_pr_counts = {}
         for repo in repos:
-            pr_count = 0
+            repo_pr_count = 0
             pr_desc = []
             for pull in repo.get_open_pulls():
                 desc = self._pull_formatter.format(pull, self._name_formatter)
                 pr_desc.append(desc)
-                pr_count += 1
+                repo_pr_count += 1
 
-            repo_desc = self._repo_formatter.format(repo, pr_count)
-            print(repo_desc)
-            for desc in pr_desc:
-                print(desc)
+            repo_pr_counts[repo.name] = repo_pr_count
+            if self._config.ignore_repos_with_zero_prs and repo_pr_count == 0:
+                continue
+
+            repo_desc = self._repo_formatter.format(repo, repo_pr_count) + "\n" + "\n".join(pr_desc)
+            repo_messages.append(repo_desc)
+
+        return "\n".join(repo_messages), repo_pr_counts
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import json
+import boto3
 import os
 
 from github import Github
@@ -7,6 +8,8 @@ from config import Config
 from models import Repo
 from formatters import NameFormatter
 from formatters.slack import PullFormatter, RepoFormatter, SlackFormatter
+
+sns_client = boto3.client('sns')
 
 
 def handler(event, context, resources=os.environ):
@@ -31,9 +34,6 @@ def handler(event, context, resources=os.environ):
     if current_org is None:
         raise Exception("Not an authorized org.")
 
-    repo_details = ""
-    total_pr_count = 0
-
     name_formatter = NameFormatter(config)
     pull_formatter = PullFormatter(config)
     repo_formatter = RepoFormatter(config)
@@ -43,20 +43,34 @@ def handler(event, context, resources=os.environ):
 
     for repo in repositories:
         repo_name = repo["repo"]
-        git_repo = current_org.get_repo(repo_name)
-        repos.append(Repo(git_repo))
+        try:
+            git_repo = current_org.get_repo(repo_name)
+            repos.append(Repo(git_repo))
+        except Exception:
+            print("Error while getting the repo. "
+                  f"Make sure the repo you mentioned('{repo}') is correct and/or you have permission to view.")
 
-    slack_formatter.format_repos(repos=repos)
+    message, pr_counts = slack_formatter.format_repos(repos=repos)
+    total_pr_count = sum(pr_counts.values())
 
     if total_pr_count == 0:
         print("No open PRs. Good job!")
         return
 
-    print("Message to SNS", repo_details)
+    print("Message to SNS: \n", message)
 
+    sns_client.publish(
+        TopicArn=sns_topic,
+        Subject=f"{total_pr_count} Pending PRs",
+        Message=message
+    )
 
-handler(None, None)
-
-
-
-
+#
+# params = {
+#     "ORGANIZATION": "trivago N.V.",
+#     "GITHUB_TOKEN": os.environ["GITHUB_TOKEN"],
+#     "SNS_TOPIC": "arn:aws:sns:eu-west-1:311937351692:consolidation--resources--ppeiris--notifications-topic",
+#     "BUCKET_NAME": "source-consolidation",
+#     "CONFIG_FILE": "configs/config.json"
+# }
+# handler(None, None, params)
