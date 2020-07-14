51ebdaeab2fc6f2aa0c3010cbd16018de3df3049
Praneeth Peiris <gnomez.grave@gmail.com>
2020-07-14 09:41:19 +0200

    User S3 path when a bucket is given

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-import json
-import boto3
 import os
 
 from github import Github
@@ -7,7 +5,7 @@ from config import Config
 
 from models import Repo
 from formatters import NameFormatter
-from formatters.html import RepoFormatter, PRFormatter
+from formatters.slack import RepoFormatter, PRFormatter
 
 from targets import SNS
 
@@ -17,14 +15,14 @@ def handler(event, context, resources=os.environ):
     token = resources["GITHUB_TOKEN"]
     sns_topic = resources.get("SNS_TOPIC")
     bucket_name = resources.get("BUCKET_NAME")
-    config_file_s3_path = resources.get("CONFIG_FILE_S3_PATH")
     config_file = resources["CONFIG_FILE"]
 
-    # Load from local
-    config = Config.load_from_local(config_file)
-
-    # If you need to load from S3, uncomment below
-    # config = Config.load_from_s3(bucket_name, config_file_s3_path)
+    if bucket_name:
+        # Load from S3
+        config = Config.load_from_s3(bucket_name, config_file)
+    else:
+        # Load from local
+        config = Config.load_from_local(config_file)
 
     repositories = config.repositories
 
@@ -65,6 +63,7 @@ def handler(event, context, resources=os.environ):
     title = f"{total_pr_count} Pending PRs"
 
     sns = SNS(sns_topic)
+    print(title, message)
     return sns.send(title, message)
 
 
@@ -73,7 +72,6 @@ def handler(event, context, resources=os.environ):
 #     "GITHUB_TOKEN": os.environ["GITHUB_TOKEN"],
 #     "SNS_TOPIC": "some sns topic arn",
 #     "BUCKET_NAME": "bucket with the config file",
-#     "CONFIG_FILE_S3_PATH": "config file name inside the bucket",
 #     "CONFIG_FILE": "configs/config.json"
 # }
 # handler(None, None, params)
